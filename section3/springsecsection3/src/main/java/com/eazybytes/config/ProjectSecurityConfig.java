a5c03d66a24bd5f6d20843a19191e6b4c2376def
rajan98 <rajan.kum.sha@gmail.com>
2024-08-05 13:56:41 +0530

    Section 3: Creating manual users using UserDetailService with noop PasswordEncoder

---
+++ section3/springsecsection3/src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
@@ -0,0 +1,48 @@
+package com.eazybytes.config;
+
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class ProjectSecurityConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        /* Here .permit All will allow all the request to pass through without authentication.
+        * Similarly we have .denyAll() that will deny all the request regardless of the authentication.
+        * */
+        /* http.authorizeHttpRequests((requests) -> requests.anyRequest().permitAll());
+        http.authorizeHttpRequests((requests) -> requests.anyRequest().denyAll()); */
+        http.authorizeHttpRequests((requests) -> requests
+                .requestMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards").authenticated()
+                .requestMatchers("/contact", "/notices", "/error").permitAll());
+
+        /* we can disable the default form login to remove the redirection to
+        * the login page. Instead it will use HTTP basic login.
+        * http.formLogin(flc -> flc.disable());
+        * To disable HTTP basic login we can use below code
+        * http.httpBasic(hbc -> hbc.disable());
+        * */
+        http.formLogin(withDefaults());
+        http.httpBasic(withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withUsername("batman").password("{noop}darknight").authorities("admin").build();
+        UserDetails user2 = User.withUsername("superman").password("{noop}clark").authorities("read").build();
+        return new InMemoryUserDetailsManager(user, user2);
+    }
+
+}
