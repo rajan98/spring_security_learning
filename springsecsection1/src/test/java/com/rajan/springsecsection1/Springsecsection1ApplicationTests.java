44484b02cc57fb391b385c3ef5b9111f341d7b25
Rajan Shah <rajan.kum.sha@gmail.com>
2025-04-30 14:51:32 +0530

    Initial Seciton 1 setup

---
+++ springsecsection1/src/test/java/com/rajan/springsecsection1/Springsecsection1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rajan.springsecsection1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EazyBankBackendApplication {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
